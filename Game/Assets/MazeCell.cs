e905db75008fb84ac999c6a454ee6a7df3b1aba1
sebdroid <sebdroid@users.noreply.github.com>
2018-05-09 23:33:35 +0100

    Testing

---
+++ Game/Assets/MazeCell.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MazeCell : MonoBehaviour {
+
+    public bool visited = false;
+    public GameObject northW, southW, eastW, westW, floor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
