1bcaebf391199700656a2062482e91c589907ce7
sebdroid <sebdroid@users.noreply.github.com>
2018-05-14 19:54:08 +0100

    Added pause menu and better text handling

---
+++ Astral/Assets/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    // Use this for initialization
+    public static bool IsPaused = false;
+
+    public GameObject pauseMenuUI;
+    public GameObject inGameUI;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+		
+	}
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        inGameUI.SetActive(true);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        inGameUI.SetActive(false);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
